dd1e842fde5f9f470df223a81a0d6eaea021ffef
GamerNoTitle <admin@bili33.top>
2022-02-10 18:51:28 +0800

    bug fix

---
+++ assets/js/GetQueryString.js
@@ -0,0 +1,12 @@
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg); //获取url中"?"符后的字符串并正则匹配
+    var context = "";
+    if (r != null)
+    context = r[2];
+    reg = null;
+    r = null;
+    return context == null || context == "" || context == "undefined" ? "" : context;
+}
+
+window.onload = window.location='https://music.163.com/song/media/outer/url?id='+GetQueryString("id")+'.mp3';
--- assets/js/download.js
+++ assets/js/download.js
@@ -1,10 +1,10 @@
 function openNew(){
     var link = document.getElementById("link").value;
-    const NeteaseReg = new RegExp('music.163.com/song/(\\d+)')
+    const NeteaseReg = new RegExp('music.163.com')
     const schemeReg = new RegExp('^https?://')
     if(NeteaseReg.test(link) && schemeReg.test(link)) {
-    var ToOpen = link.replace("music.163.com", "music.ninym.top").replace("/#/","/");  //防止解析暴毙
-    window.open(ToOpen);
+        var ToOpen = link.replace("music.163.com", "music.ninym.top").replace("/#/","/");  //防止解析暴毙
+        window.open(ToOpen);
     }
     else{
         alert('请输入正确的网易云链接！\n例如 https://music.163.com/song?id=467787951&userid=252340012')
